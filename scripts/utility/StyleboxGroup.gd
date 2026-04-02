5046a302ca41c9647b364b0c86e5b23730355bd2
Nader <34183685+gitMickehh@users.noreply.github.com>
2026-04-02 15:34:58 -0700

    added stylebox group! good strructure! :D

---
+++ scripts/utility/StyleboxGroup.gd
@@ -0,0 +1,7 @@
+@icon("res://art/editor/icon_stylebox_group.png")
+class_name Stylebox_Group
+extends Resource
+
+@export var normal_stylebox: StyleBox
+@export var hover_stylebox: StyleBox
+@export var disabled_stylebox: StyleBox
